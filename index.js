902b8595b70e3a516e8b941520e3884b6beb8248
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-02-11 15:58:16 -0500

    Add nodemon, prettier

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
     return cooked;
 };
-var _a = require('apollo-server'), ApolloServer = _a.ApolloServer, gql = _a.gql;
+var _a = require("apollo-server"), ApolloServer = _a.ApolloServer, gql = _a.gql;
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
 var books = [
     {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J.K. Rowling"
     },
     {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
-    },
+        title: "Jurassic Park",
+        author: "Michael Crichton"
+    }
 ];
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
@@ -23,8 +23,8 @@ var typeDefs = gql(__makeTemplateObject(["\n  # Comments in GraphQL are defined 
 // schema.  We'll retrieve books from the "books" array above.
 var resolvers = {
     Query: {
-        books: function () { return books; },
-    },
+        books: function () { return books; }
+    }
 };
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
 const books = [
-    {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
-    },
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
-    },
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
 ];
 
 // Type definitions define the "shape" of your data and specify
@@ -36,9 +36,9 @@ const typeDefs = gql`
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
-    Query: {
-        books: () => books,
-    },
+  Query: {
+    books: () => books
+  }
 };
 
 // In the most basic sense, the ApolloServer can be started
@@ -49,5 +49,5 @@ const server = new ApolloServer({ typeDefs, resolvers });
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });
