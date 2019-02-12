56fe4cbb725664d3bf9e690e07abdbc99ffd3f52
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-02-11 22:04:23 -0500

    Add schema, enable default mocking, move .ts files to src/, move compiled .js files to lib/, misc?

---
+++ src/schema.ts
@@ -0,0 +1,49 @@
+import { gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    nodes: [Node]!
+    node(id: ID!): Node
+  }
+
+  type Node {
+    id: ID!
+    type: NodeType!
+    name: String
+    pose: NodePose
+    telemetry: NodeTelemetry
+  }
+
+  enum NodeType {
+    BASE
+    FIXED
+    MOBILE
+  }
+
+  type NodePose {
+    position: NodePosition
+    orientation: NodeOrientation
+  }
+
+  type NodePosition {
+    lat: Float!
+    lon: Float!
+  }
+
+  type NodeOrientation {
+    heading: Int!
+    source: NodeOrientationSource
+  }
+
+  enum NodeOrientationSource {
+    POSITION
+    TELEMETRY
+  }
+
+  type NodeTelemetry {
+    groundSpeed: Float
+    # Other telemetry data goes here
+  }
+`;
+
+export default typeDefs;
